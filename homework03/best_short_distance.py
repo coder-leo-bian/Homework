d8e5724e8b78545ae5afd4dda93212bc84b8ce7e
边境 <bianjing@heqi-inc.com>
2019-07-18 18:56:10 +0800

    best.py

---
+++ homework03/best_short_distance.py
@@ -0,0 +1,22 @@
+import random
+import matplotlib.pyplot as plt
+
+
+x_ = [random.random() * 200 -100 for i in range(20)]
+y_ = [random.random() * 200 -100 for i in range(20)]
+index = [i for i in range(20)]
+
+
+def best_short_distance(x_, y_):
+
+    open = []   # 待执行的点
+    closed = []     # 执行过的点
+    pathes = []  # 路径
+
+
+
+
+plt.Figure()
+plt.scatter()
+plt.show()
+
